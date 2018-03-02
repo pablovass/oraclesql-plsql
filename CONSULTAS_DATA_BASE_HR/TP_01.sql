b4db8e17b665daecb8faade0f04eea2de0b7690c
Pablo Vallejos <faktory.fm@live.com>
2018-03-02 10:12:48 -0300

    JOIN
    JOIN

--- CONSULTAS_DATA_BASE_HR/TP_01.sql
+++ CONSULTAS_DATA_BASE_HR/TP_01.sql
@@ -22,3 +22,16 @@ SELECT LAST_NAME, FIRST_NAME, HIRE_DATE, SALARY
 FROM EMPLOYEES
 WHERE HIRE_DATE LIKE '%2004' AND SALARY >=10000;
 
+/*
+Elaborar una sentencia SQL que muestre el nombre de cada departamento y el
+nombre y apellido del manager asignado
+*/
+SELECT D.DEPARTMENT_NAME, FIRST_NAME, LAST_NAME 
+FROM DEPARTMENTS D LEFT JOIN EMPLOYEES E ON D.MANAGER_ID=E.EMPLOYEE_ID;
+
+desc DEPARTMENTS;
+desc EMPLOYEES;
+-- esta no te muestra los que tienen valores nulos 
+SELECT DEPARTMENT_NAME, FIRST_NAME, LAST_NAME
+from DEPARTMENTS d join EMPLOYEES E
+ON d.MANAGER_ID=E.EMPLOYEE_ID;
