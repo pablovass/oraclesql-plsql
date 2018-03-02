f0c78fcf4e568807ab9d497481989242594967e6
Pablo Vallejos <faktory.fm@live.com>
2018-03-02 06:04:35 -0300

    ACTIVIDADES Y TEORIA
    ACTIVIDADES Y TEORIA

--- CONSULTAS_DATA_BASE_HR/TP_01.sql
+++ CONSULTAS_DATA_BASE_HR/TP_01.sql
@@ -6,3 +6,19 @@ SELECT LAST_NAME, FIRST_NAME, HIRE_DATE
 FROM EMPLOYEES
 WHERE HIRE_DATE BETWEEN TO_DATE('01/01/2006','DD/MM/YYYY')and TO_DATE('31/12/2006','DD/MM/YYYY')   ;
 
+/*
+Elaborar una sentencia SQL que muestre los empleados que perciban un salario
+menor a 10000
+*/
+
+SELECT LAST_NAME, FIRST_NAME, SALARY
+FROM EMPLOYEES
+WHERE SALARY <10000   ;
+/*
+Elaborar una sentencia SQL que muestre los empleados que hayan sido contratados
+en el año 2004 y que también perciban un salario mayor o igual a 10000.
+*/
+SELECT LAST_NAME, FIRST_NAME, HIRE_DATE, SALARY
+FROM EMPLOYEES
+WHERE HIRE_DATE LIKE '%2004' AND SALARY >=10000;
+
